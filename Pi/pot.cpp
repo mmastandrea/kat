6a8fc82cc256a18a8ada95c584eef81c92c36e00
captaincheese <captaincheese@themastandreas.com>
2015-03-21 18:29:39 -0700

    Revert "Test Change 2: Return of the .exe"
    This reverts commit 92323e67d7d7f7eb64cf8ddf452aaccefbd68c49.

---
+++ Pi/pot.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+char mew = "";
+
+
+
+int main()
+{
+  std::cout << " y or n?" << std::endl;
+  if(mew == "y")
+    std::cout << "Good" << std::endl;
+  else
+    std::cout << "FUCK OFF TEHN YA EGG!!" << std::endl;
+  char j;
+  std::cin >> j;
+  return 0;
+}
